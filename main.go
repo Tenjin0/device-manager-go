2e91c0683efa07aa1cae41836530dfc558763bd6
Patrice Petit <ppetit@wynd.eu>
2019-10-04 10:23:03 +0200

    feature/core: try another socketIO

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
-	socketio "github.com/googollee/go-socket.io"
+	gosocketio "github.com/graarh/golang-socketio"
+	"github.com/graarh/golang-socketio/transport"
 )
 
 // var wsupgrader = websocket.Upgrader{
@@ -17,52 +17,42 @@ import (
 // 	},
 // }
 
-func wshandler(w http.ResponseWriter, r *http.Request) {
+func wshandler() *gosocketio.Server {
+	server := gosocketio.NewServer(transport.GetDefaultWebsocketTransport())
 
-	server, err := socketio.NewServer(nil)
-	if err != nil {
-		log.Fatal(err)
+	//handle connected
+	server.On(gosocketio.OnConnection, func(c *gosocketio.Channel) {
+		log.Println("New client connected")
+		//join them to room
+		c.Join("chat")
+	})
+
+	type Message struct {
+		Name    string `json:"name"`
+		Message string `json:"message"`
 	}
-	server.OnConnect("/", func(s socketio.Conn) error {
-		s.SetContext("")
-		fmt.Println("connected:", s.ID())
-		return nil
-	})
-	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
-		fmt.Println("notice:", msg)
-		s.Emit("reply", "have "+msg)
-	})
-	server.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
-		s.SetContext(msg)
-		return "recv " + msg
-	})
-	server.OnEvent("/", "bye", func(s socketio.Conn) string {
-		last := s.Context().(string)
-		s.Emit("bye", last)
-		s.Close()
-		return last
-	})
-	server.OnError("/", func(e error) {
-		fmt.Println("meet error:", e)
-	})
-	server.OnDisconnect("/", func(s socketio.Conn, msg string) {
-		fmt.Println("closed", msg)
+
+	//handle custom event
+	server.On("send", func(c *gosocketio.Channel, msg Message) string {
+		fmt.Println(msg)
+		//send event to all in room
+		return "OK"
 	})
 
-	go server.ServeHTTP(w, r)
+	//setup http server
+
+	return server
 }
 
 func main() {
 
 	r := gin.Default()
+	// server := wshandler()
 
-	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello")
-	})
+	r.StaticFile("/", "./asset")
 
 	r.GET("/socket.io/", func(c *gin.Context) {
-
-		wshandler(c.Writer, c.Request)
+		c.String(200, "Hello")
 	})
 
 	r.Run("localhost:1234")
